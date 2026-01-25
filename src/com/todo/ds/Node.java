1e782d3d5270f0b3c797226d8b35fe27794c8558
Rashmika Perera <144840582+rashmika-perera@users.noreply.github.com>
2026-01-25 11:48:25 +0530

    Make Node class generic and add fields
    Refactored Node to be a generic class with type parameter T. Added public fields for data, prev, and next, and implemented a constructor to initialize data.

--- src/com/todo/ds/Node.java
+++ src/com/todo/ds/Node.java
@@ -1,5 +1,11 @@
 package com.todo.ds;
 
-public class Node {
+public class Node<T> {
+    public T data;
+    public Node<T> prev;
+    public Node<T> next;
 
+    public Node(T data) {
+        this.data = data;
+    }
 }
