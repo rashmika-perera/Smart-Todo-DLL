fc976107ce9ac5f6dca7fd0c999f88f4fd5d7e2a
Rashmika Perera <144840582+rashmika-perera@users.noreply.github.com>
2026-01-26 09:41:03 +0530

    Refactor DoublyLinkedList to be generic and add utilities
    Converted DoublyLinkedList to a generic class, added head/tail accessors, size tracking, and utility methods for adding, removing, iterating, and finding nodes. This improves flexibility and usability of the data structure.

--- src/com/todo/ds/DoublyLinkedList.java
+++ src/com/todo/ds/DoublyLinkedList.java
@@ -1,5 +1,59 @@
 package com.todo.ds;
 
-public class DoublyLinkedList {
+import java.util.function.Consumer;
 
+public class DoublyLinkedList<T> {
+    private Node<T> head;
+    private Node<T> tail;
+    private int size = 0;
+
+    public Node<T> getHead() { return head; }
+    public Node<T> getTail() { return tail; }
+    public int size() { return size; }
+    public boolean isEmpty() { return size == 0; }
+
+    public Node<T> addLast(T data) {
+        Node<T> node = new Node<>(data);
+        if (head == null) {
+            head = tail = node;
+        } else {
+            tail.next = node;
+            node.prev = tail;
+            tail = node;
+        }
+        size++;
+        return node;
+    }
+
+    public void removeNode(Node<T> node) {
+        if (node == null) return;
+
+        if (node == head) head = node.next;
+        if (node == tail) tail = node.prev;
+
+        if (node.prev != null) node.prev.next = node.next;
+        if (node.next != null) node.next.prev = node.prev;
+
+        node.prev = null;
+        node.next = null;
+        size--;
+    }
+
+    public void forEach(Consumer<T> action) {
+        Node<T> cur = head;
+        while (cur != null) {
+            action.accept(cur.data);
+            cur = cur.next;
+        }
+    }
+
+    // helper: find node by predicate (simple)
+    public Node<T> find(java.util.function.Predicate<T> predicate) {
+        Node<T> cur = head;
+        while (cur != null) {
+            if (predicate.test(cur.data)) return cur;
+            cur = cur.next;
+        }
+        return null;
+    }
 }
